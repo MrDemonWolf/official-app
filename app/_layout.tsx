d761c9bdbc325922601d5a2586876bd7600f0ef2
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2025-11-25 01:31:05 -0600

    Feat: Integrate Sentry for error tracking and session replay

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,8 +15,28 @@ import colors from "tailwindcss/colors";
 import "@/global.css";
 
 import "react-native-reanimated";
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://332056eee148326785189286ec6d77a2@o4508281688752128.ingest.us.sentry.io/4510419880640512',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const safeAreaInsets = useSafeAreaInsets();
 
@@ -81,4 +101,4 @@ export default function RootLayout() {
       <StatusBar style="auto" />
     </ThemeProvider>
   );
-}
+});
