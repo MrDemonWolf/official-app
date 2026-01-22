2f655bd5f3cff8fcd778f3bfac527eab203f2636
Nathanial Henniges <19924836+nathanialhenniges@users.noreply.github.com>
2026-01-22 04:22:35 -0600

    chore: untrack native folders and keep locally

--- app.config.js
+++ app.config.js
@@ -1,103 +1,76 @@
-// Expo app config using EAS environment variables
 const IS_DEV = process.env.APP_VARIANT === "development";
-const IS_PREVIEW = process.env.APP_VARIANT === "preview";
-
-const getUniqueIdentifier = () => {
-  if (IS_DEV) {
-    return "com.mrdemonwolf.OfficialApp.dev";
-  }
-  if (IS_PREVIEW) {
-    return "com.mrdemonwolf.OfficialApp.preview";
-  }
-  return "com.mrdemonwolf.OfficialApp";
-};
-
-const getAppName = () => {
-  if (IS_DEV) {
-    return "MrDemonWolf (Dev)";
-  }
-  if (IS_PREVIEW) {
-    return "MrDemonWolf (Preview)";
-  }
-  return "MrDemonWolf";
-};
 
 export default {
-  name: getAppName(),
-  slug: "mrdemonwolf",
-  version: "1.0.0",
-  // Allow all orientations by default. Change to "portrait" or
-  // "landscape" to lock if desired. Setting to "default" lets
-  // the device choose and enables landscape where supported.
-  orientation: "default",
-  icon: "./assets/images/icon.png",
-  scheme: "mrdemonwolf",
-  userInterfaceStyle: "automatic",
-  newArchEnabled: true,
-  ios: {
-    supportsTablet: true,
-    bundleIdentifier: getUniqueIdentifier(),
-    icon: "./assets/mrdemonwolf-ios.icon",
-    infoPlist: {
-      ITSAppUsesNonExemptEncryption: false,
+  expo: {
+    name: IS_DEV ? "MrDemonWolf (Dev)" : "MrDemonWolf",
+    slug: "mrdemonwolf",
+    version: "1.0.0",
+    scheme: "mrdemonwolf",
+    jsEngine: "hermes",
+    newArchEnabled: true,
+    runtimeVersion: {
+      policy: "appVersion",
     },
-    buildNumber: "6",
-  },
-  android: {
-    adaptiveIcon: {
-      backgroundColor: "#E6F4FE",
-      foregroundImage: "./assets/images/android/android-icon-foreground.png",
-      backgroundImage: "./assets/images/android/android-icon-background.png",
-      monochromeImage: "./assets/images/android/android-icon-monochrome.png",
-    },
-    edgeToEdgeEnabled: true,
-    predictiveBackGestureEnabled: false,
-    intentFilters: [
-      {
-        action: "VIEW",
-        data: [
+    userInterfaceStyle: "automatic",
+    backgroundColor: "#FFFFFF",
+    icon: "./assets/images/icon.png",
+    ios: {
+      icon: "./assets/mrdemonwolf-ios.icon",
+      bundleIdentifier: "com.mrdemonwolf.OfficialApp",
+      supportsTablet: true,
+      privacyManifests: {
+        NSPrivacyAccessedAPITypes: [
           {
-            scheme: "mrdemonwolf",
+            NSPrivacyAccessedAPIType:
+              "NSPrivacyAccessedAPICategoryUserDefaults",
+            NSPrivacyAccessedAPITypeReasons: ["CA92.1"],
           },
         ],
-        category: ["BROWSABLE", "DEFAULT"],
       },
-    ],
-    package: getUniqueIdentifier(),
-  },
-  plugins: [
-    "expo-router",
-    [
-      "expo-splash-screen",
-      {
-        image: "./assets/images/splash-icon.png",
-        imageWidth: 200,
-        resizeMode: "contain",
-        backgroundColor: "#ffffff",
-        dark: {
-          backgroundColor: "#000000",
-        },
-      },
-    ],
-    [
-      "@sentry/react-native/expo",
-      {
-        url: "https://sentry.io/",
-        project: "officialapp",
-        organization: "mrdemonwolf",
-      },
-    ],
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
-  extra: {
-    router: {},
-    eas: {
-      appVariant: process.env.APP_VARIANT,
-      projectId: "e10e7345-9182-468c-beb1-f7c15c2c813e",
     },
+    android: {
+      package: "com.mrdemonwolf.OfficialApp",
+      softwareKeyboardLayoutMode: "resize",
+      // adaptiveIcon: {
+      //   backgroundColor: "#E6F4FE",
+      //   foregroundImage: "./assets/images/android-icon-foreground.png",
+      // },
+      permissions: ["INTERNET"],
+    },
+    extra: {
+      eas: {
+        projectId: "4a220b17-d746-48f1-9f46-d83a0a933b40",
+      },
+      apiUrl: process.env.EXPO_PUBLIC_API_URL,
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            image: "./assets/images/splash-icon.png",
+            backgroundColor: "#0B1226",
+          },
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          android: {
+            compileSdkVersion: 35,
+            targetSdkVersion: 35,
+          },
+          ios: {
+            deploymentTarget: "18.0",
+          },
+        },
+      ],
+    ],
+    owner: "mrdemonwolf-org",
   },
-  owner: "mrdemonwolf-org",
 };
